7a83d0ccc3710201052d48da99c6d71d21cb0c6d
yanganan <13640662396@163.com>
2018-01-12 00:03:52 +0800

    anko引入，以及调试使用

--- usercenter/src/main/java/com/yan/user/ui/activity/RegisterActivity.kt
+++ usercenter/src/main/java/com/yan/user/ui/activity/RegisterActivity.kt
@@ -3,6 +3,8 @@ package com.yan.user.ui.activity
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.yan.user.R
+import kotlinx.android.synthetic.main.activity_register.*
+import org.jetbrains.anko.startActivity
 
 /**
  *  @author      : yan
@@ -14,5 +16,9 @@ class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        mRegisterBtn.setOnClickListener {
+            startActivity<TestActivity>("id" to 10)
+        }
     }
 }
