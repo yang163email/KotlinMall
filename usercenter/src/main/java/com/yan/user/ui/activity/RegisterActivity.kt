6556bbd5aa1e6fbc1a93d401dfa034dfe292d228
yanganan <13640662396@163.com>
2018-01-11 22:42:19 +0800

    切换分支到dev；初始化用户模块

---
+++ usercenter/src/main/java/com/yan/user/ui/activity/RegisterActivity.kt
@@ -0,0 +1,18 @@
+package com.yan.user.ui.activity
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.yan.user.R
+
+/**
+ *  @author      : yan
+ *  @date        : 2018/1/11 22:34
+ *  @description : 注册界面
+ */
+class RegisterActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
