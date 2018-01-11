c22a7f0462560abadb47e827e4aaefc58415e4f9
yanganan <13640662396@163.com>
2018-01-11 22:12:39 +0800

    初始化项目与包结构设计

---
+++ app/src/main/java/com/yan/mall/ui/activity/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.yan.mall.ui.activity
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.yan.mall.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
