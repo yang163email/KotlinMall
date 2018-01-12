92b110409944e6a72a58f45590363a5631945690
yanganan <13640662396@163.com>
2018-01-12 17:44:39 +0800

    Retrofit集成及单例工厂

---
+++ baselibrary/src/main/java/com/yan/base/common/BaseConstant.kt
@@ -0,0 +1,14 @@
+package com.yan.base.common
+
+/**
+ *  @author      : yan
+ *  @date        : 2018/1/12 17:26
+ *  @description : baseLibrary中的常量类
+ */
+object BaseConstant {
+    /** 服务器地址 */
+    const val SERVER_ADDRESS = ""
+
+    const val CONNECT_TIMEOUT = 10L
+    const val READ_TIMEOUT = 10L
+}
---
+++ baselibrary/src/main/java/com/yan/base/data/net/RetrofitFactory.kt
@@ -0,0 +1,73 @@
+package com.yan.base.data.net
+
+import com.yan.base.common.BaseConstant
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+
+/**
+ *  @author      : yan
+ *  @date        : 2018/1/12 17:24
+ *  @description : Retrofit工厂单例类
+ */
+class RetrofitFactory private constructor(){
+
+    companion object {
+        val instance by lazy { RetrofitFactory() }
+    }
+
+    private val retrofit: Retrofit
+    private val interceptor: Interceptor
+
+    init {
+        interceptor = initInterceptor()
+
+        retrofit = Retrofit.Builder()
+                .baseUrl(BaseConstant.SERVER_ADDRESS)
+                .addConverterFactory(GsonConverterFactory.create())
+                .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
+                .client(initClient())
+                .build()
+    }
+
+    /**
+     * OkHttpClient
+     */
+    private fun initClient(): OkHttpClient =
+        OkHttpClient.Builder()
+                .addInterceptor(initLogInterceptor())
+                .addInterceptor(interceptor)
+                .connectTimeout(BaseConstant.CONNECT_TIMEOUT, TimeUnit.SECONDS)
+                .readTimeout(BaseConstant.READ_TIMEOUT, TimeUnit.SECONDS)
+                .build()
+
+    /**
+     * 其他统一拦截器
+     */
+    private fun initInterceptor() = Interceptor { chain ->
+        val request = chain.request()
+                .newBuilder()
+                .addHeader("Content-Type", "application/json")
+                .addHeader("charset", "utf-8")
+                .build()
+        chain.proceed(request)
+    }
+
+    /**
+     * 日志拦截器
+     */
+    private fun initLogInterceptor(): Interceptor {
+        val interceptor = HttpLoggingInterceptor()
+        interceptor.level = HttpLoggingInterceptor.Level.BODY
+        return interceptor
+    }
+
+    /**
+     * 暴露方法，返回Service对象
+     */
+    fun <T> create(service: Class<T>) = retrofit.create(service)
+}
