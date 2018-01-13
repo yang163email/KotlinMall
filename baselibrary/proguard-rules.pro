d2b4be13b7c43879d31551bd90b58f4ea44894bb
yanganan <13640662396@163.com>
2018-01-13 22:52:17 +0800

    TakePhoto选择图片功能开发

--- baselibrary/proguard-rules.pro
+++ baselibrary/proguard-rules.pro
@@ -37,3 +37,13 @@
     **[] $VALUES;
     public *;
 }
+
+# TakePhoto
+-keep class com.jph.takephoto.** { *; }
+-dontwarn com.jph.takephoto.**
+
+-keep class com.darsh.multipleimageselect.** { *; }
+-dontwarn com.darsh.multipleimageselect.**
+
+-keep class com.soundcloud.android.crop.** { *; }
+-dontwarn com.soundcloud.android.crop.**
--- usercenter/src/main/java/com/yan/user/ui/activity/UserInfoActivity.kt
+++ usercenter/src/main/java/com/yan/user/ui/activity/UserInfoActivity.kt
@@ -1,31 +1,54 @@
 package com.yan.user.ui.activity
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.os.Environment
+import android.util.Log
 import com.bigkoo.alertview.AlertView
+import com.jph.takephoto.app.TakePhoto
+import com.jph.takephoto.app.TakePhotoImpl
+import com.jph.takephoto.compress.CompressConfig
+import com.jph.takephoto.model.InvokeParam
+import com.jph.takephoto.model.TContextWrap
+import com.jph.takephoto.model.TResult
+import com.jph.takephoto.permission.InvokeListener
+import com.jph.takephoto.permission.PermissionManager
+import com.jph.takephoto.permission.PermissionManager.TPermissionType
+import com.jph.takephoto.permission.TakePhotoInvocationHandler
 import com.yan.base.ext.onClick
 import com.yan.base.ui.activity.BaseMvpActivity
+import com.yan.base.utils.DateUtils
 import com.yan.user.R
 import com.yan.user.injection.component.DaggerUserComponent
 import com.yan.user.injection.module.UserModule
 import com.yan.user.presenter.UserInfoPresenter
 import com.yan.user.presenter.view.UserInfoView
 import kotlinx.android.synthetic.main.activity_user_info.*
-import org.jetbrains.anko.toast
+import java.io.File
 
 /**
  *  @author      : yan
  *  @date        : 2018/1/11 22:34
  *  @description : 用户信息界面
  */
-class UserInfoActivity : BaseMvpActivity<UserInfoPresenter>(), UserInfoView {
+class UserInfoActivity : BaseMvpActivity<UserInfoPresenter>(), UserInfoView,
+        TakePhoto.TakeResultListener, InvokeListener {
+
+    val TAG = javaClass.simpleName
 
     private var alertView: AlertView? = null
+    private lateinit var mTakePhoto: TakePhoto
+    private lateinit var mTempFile: File
+    private var invokeParam: InvokeParam? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user_info)
 
+        getTakePhoto()
         initView()
+        mTakePhoto.onCreate(savedInstanceState)
     }
 
     private fun initView() {
@@ -44,9 +67,13 @@ class UserInfoActivity : BaseMvpActivity<UserInfoPresenter>(), UserInfoView {
                 .setDestructive("拍照", "相册")
                 .setOthers(null)
                 .setOnItemClickListener { o, position ->
+                    mTakePhoto.onEnableCompress(CompressConfig.ofDefaultConfig(), false)
                     when (position) {
-                        0 -> toast("拍照")
-                        1 -> toast("相册")
+                        0 -> {
+                            createTempFile()
+                            mTakePhoto.onPickFromCapture(Uri.fromFile(mTempFile))
+                        }
+                        1 -> mTakePhoto.onPickFromGallery()
                     }
                 }
                 .build()
@@ -61,6 +88,56 @@ class UserInfoActivity : BaseMvpActivity<UserInfoPresenter>(), UserInfoView {
                 .build()
                 .inject(this)
         mPresenter.mView = this
+    }
+
+    private fun createTempFile() {
+        val tempFileName = "${DateUtils.curTime}.jpg"
+        if (Environment.MEDIA_MOUNTED == Environment.getExternalStorageState()) {
+            mTempFile = File(Environment.getExternalStorageDirectory(), tempFileName)
+            return
+        }
+        mTempFile = File(cacheDir, tempFileName)
+    }
+
+    override fun takeSuccess(result: TResult?) {
+        Log.d(TAG, "takeSuccess: ${result?.image?.originalPath}")
+        Log.d(TAG, "takeSuccess: ${result?.image?.compressPath}")
+    }
+
+    override fun takeCancel() {
+        
+    }
+
+    override fun takeFail(result: TResult?, msg: String?) {
+        Log.d(TAG, "takeFail: $msg")
+    }
+
+    override fun invoke(invokeParam: InvokeParam): TPermissionType {
+        val type = PermissionManager.checkPermission(TContextWrap.of(this), invokeParam.method)
+        if (TPermissionType.WAIT == type) {
+            this.invokeParam = invokeParam
+        }
+        return type
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        mTakePhoto.onActivityResult(requestCode, resultCode, data)
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        //以下代码为处理Android6.0、7.0动态权限所需
+        val type = PermissionManager.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        PermissionManager.handlePermissionsResult(this, type, invokeParam, this)
+    }
+
+    /**
+     * 获取TakePhoto实例
+     * @return
+     */
+    fun getTakePhoto() {
+        mTakePhoto = TakePhotoInvocationHandler.of(this).bind(TakePhotoImpl(this, this)) as TakePhoto
     }
 
     override fun onBackPressed() {
