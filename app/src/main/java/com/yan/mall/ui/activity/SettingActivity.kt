24e093afe5ad949141fb7e1b03b903ad17018a93
yanganan <13640662396@163.com>
2018-01-14 22:25:06 +0800

    设置界面点击事件

--- app/src/main/java/com/yan/mall/ui/activity/SettingActivity.kt
+++ app/src/main/java/com/yan/mall/ui/activity/SettingActivity.kt
@@ -7,6 +7,7 @@ import com.yan.base.ui.activity.BaseActivity
 import com.yan.mall.R
 import com.yan.user.utils.UserPrefsUtils
 import kotlinx.android.synthetic.main.activity_setting.*
+import org.jetbrains.anko.toast
 
 /**
  *  @author      : yan
@@ -23,11 +24,17 @@ class SettingActivity : BaseActivity(), View.OnClickListener{
     }
 
     private fun initView() {
+        mTvUserProtocol.onClick(this)
+        mTvFeedBack.onClick(this)
+        mTvAbout.onClick(this)
         mBtnLogout.onClick(this)
     }
 
     override fun onClick(v: View) {
         when(v.id) {
+            R.id.mTvUserProtocol -> toast("用户协议")
+            R.id.mTvFeedBack -> toast("反馈意见")
+            R.id.mTvAbout -> toast("关于")
             R.id.mBtnLogout -> {
                 UserPrefsUtils.putUserInfo(null)
                 finish()
