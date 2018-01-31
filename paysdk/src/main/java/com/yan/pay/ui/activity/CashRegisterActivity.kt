9b06e6cf651af4747d7389b2e614ce3203338c2e
yanganan <13640662396@163.com>
2018-01-31 20:10:50 +0800

    支付宝sdk支付订单，沙箱模式

--- paysdk/src/main/java/com/yan/pay/ui/activity/CashRegisterActivity.kt
+++ paysdk/src/main/java/com/yan/pay/ui/activity/CashRegisterActivity.kt
@@ -1,8 +1,9 @@
 package com.yan.pay.ui.activity
 
 import android.os.Bundle
-import com.alibaba.android.arouter.facade.annotation.Autowired
 import com.alibaba.android.arouter.facade.annotation.Route
+import com.alipay.sdk.app.EnvUtils
+import com.alipay.sdk.app.PayTask
 import com.kotlin.base.utils.YuanFenConverter
 import com.yan.base.ext.onClick
 import com.yan.base.ui.activity.BaseMvpActivity
@@ -14,7 +15,9 @@ import com.yan.pay.presenter.view.PayView
 import com.yan.provider.common.ProviderConstant
 import com.yan.provider.router.RouterPath
 import kotlinx.android.synthetic.main.activity_cash_register.*
+import org.jetbrains.anko.doAsync
 import org.jetbrains.anko.toast
+import org.jetbrains.anko.uiThread
 
 /**
  *  @author      : yan
@@ -24,24 +27,16 @@ import org.jetbrains.anko.toast
 @Route(path = RouterPath.PaySDK.PATH_PAY)
 class CashRegisterActivity : BaseMvpActivity<PayPresenter>(), PayView {
 
-    @Autowired(name = ProviderConstant.KEY_ORDER_ID)
-    @JvmField
-    var mOrderId = 0
-
-    @Autowired(name = ProviderConstant.KEY_ORDER_PRICE)
-    @JvmField
-    var mTotalPrice = 0L
+    private var mOrderId = 0
+    private var mTotalPrice: Long = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cash_register)
 
+        EnvUtils.setEnv(EnvUtils.EnvEnum.SANDBOX)
+        initData()
         initView()
-    }
-
-    private fun initView() {
-        mTvTotalPrice.text = YuanFenConverter.changeF2YWithUnit(mTotalPrice)
-        mBtnPay.onClick { mPresenter.getPaySign(mOrderId, mTotalPrice) }
     }
 
     override fun injectComponent() {
@@ -53,7 +48,26 @@ class CashRegisterActivity : BaseMvpActivity<PayPresenter>(), PayView {
         mPresenter.mView = this
     }
 
+    private fun initData() {
+        mOrderId = intent.getIntExtra(ProviderConstant.KEY_ORDER_ID, -1)
+        mTotalPrice = intent.getLongExtra(ProviderConstant.KEY_ORDER_PRICE, -1L)
+    }
+
+    private fun initView() {
+        mTvTotalPrice.text = YuanFenConverter.changeF2YWithUnit(mTotalPrice)
+        mBtnPay.onClick { mPresenter.getPaySign(mOrderId, mTotalPrice) }
+    }
+
     override fun onGetSignResult(result: String) {
-        toast(result)
+        doAsync {
+            val resultMap = PayTask(this@CashRegisterActivity).payV2(result, true)
+            uiThread {
+                if ("9000" == resultMap["resultStatus"]) {
+                    toast("支付成功")
+                } else {
+                    toast("支付失败${resultMap["memo"]}")
+                }
+            }
+        }
     }
 }
